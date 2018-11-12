2f01f64e44042b77efe0daec7c9940e48a575222
Daniel Pérez <dpsol@users.noreply.github.com>
2018-11-12 16:56:14 -0600

    Ligas
    Primer tema terminado

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+//https://engineering.musefind.com/build-your-first-progressive-web-app-with-react-8e1449c575cd
+//https://dev.to/aurelkurtula/introduction-to-react-532
+//react router redirect after login
+// https://dev.to/aurelkurtula/introduction-to-react-router-ha
